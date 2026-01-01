fc972d8e94b75dc2a11f28ecfb58e4729a97eff6
oshadhaCperera <oshadhacperera999@gmail.com>
2026-01-01 11:48:52 +0530

    8th try

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 export default function Home() {
-  let quote = "Consistency beats motivation.";
+  let quote = "Consistency beats motivation.another try";
   return (
 
     <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Daily Quote from oshadha testing fail from eslint</h1>
+      <h1>Daily Quote from oshadha testing fail from eslint 2nd try</h1>
       <p>"Consistency beats motivation."</p>
     </main>
   );
