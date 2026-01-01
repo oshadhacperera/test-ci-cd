19b81344f0621e5397a8d683a5bacabf7c68384d
oshadhaCperera <oshadhacperera999@gmail.com>
2026-01-01 12:49:29 +0530

    16

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Daily Quote from oshadha testing fail from eslint 15th try</h1>
+      <h1>Daily Quote from oshadha testing fail from eslint 16th try</h1>
       <p>"Consistency beats motivation."</p>
     </main>
   );
